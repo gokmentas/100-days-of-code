6c3f2c49ac16ef78bd243fd1a03b34baa2a93066
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-08-11 17:16:02 +0300

    testing

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,6 @@
-# Write your code below this line 👇
-print("Hello world!")
+print("Welcome to the Brand Name Genarator.")
+
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
