fcf9ced74a42da901afdf4b7f95ede5f0be33866
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-08-14 12:43:49 +0300

    Day 3 completed

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -9,7 +9,7 @@ print('''
 |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
 |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+ _________|___________| ;`-.o`"=._; ." ` '`." ` . "-._ /_______________|_______
 |                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
 |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
 ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
@@ -22,29 +22,44 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-choice1 = input('You\'re at a crossroad, where do you want to go? '
-                'Type "left" or "right".\n').lower()
 
-if choice1 == "left":
-    choice2 = input('You\'ve come to a lake. '
-                    'There is an island in the middle of the lake. '
-                    'Type "wait" to wait for a boat. '
-                    'Type "swim" to swim across.\n').lower()
-    if choice2 == "wait":
-        choice3 = input("You arrive at the island unharmed. "
-                        "There is house with 3 doors. One red, "
-                        "one yellow and one blue. "
-                        "Which colour do you choose?\n").lower()
-        if choice3 == "red":
-            print("It's a room full of fire. Game Over")
-        elif choice3 == "yellow":
-            print("You found the treasure. You Win!")
-        elif choice3 == "blue":
-            print("You enter a room of beasts. Game Over.")
-        else:
-            print("You chose a door that doesn't exist. Game Over.")
-    else:
-        print("You got attacked by an angry trout. Game Over.")
-
+choice1 = input("You see a beautiful women on the road. "
+                "Say Hi to her: 1 "
+                "Get pass her: 2 ")
+if choice1 == "1":
+    print("She stabbed you with a knife and you died.")
 else:
-    print("You fell in to a hole. Game Over.")
+    choice2 = input("2 paths ahead. One leads to the jungle and other one leads to a river. "
+                    "Type jungle or river: ").lower()
+    if choice2 == "jungle":
+        choice3 = input("You see something on the floor. "
+                        "You can dig the spot with a shovel, throw a rock, get pass it. "
+                        "Type shovel, rock or pass: ").lower()
+        if choice3 == "shovel":
+            print("A bomb exploded and you died.")
+        elif choice3 == "rock":
+            print("A bomb exploded and your leg chopped off. You died because of bleeding.")
+        else:
+            choice4 = input("Finally you see a treasure chest but a mouse sitting on top of it. "
+                            "Type reason to try reason with her, "
+                            "type kiss to kiss the mouse, "
+                            "type yell to try scare the mouse, "
+                            "type anything you want to say to her,\n"
+                            "type attack to try attacking her: ").lower()
+            if choice4 == "reason":
+                print("It gets angry and eats you alive.")
+            elif choice4 == "kiss":
+                print("She turns to a fine lady and turns out "
+                      "you have met your soulmate. You won.")
+            elif choice4 == "yell":
+                print("It gets angry and eats you alive.")
+            elif choice4 == "attack":
+                choice5 = input("Use your shovel or your fists. 1 or 2: ")
+                if choice5 == "1":
+                    print("You killed the mouse and took the treasure. You won.")
+                else:
+                    print("It was so fast. You couldn't even touch it before it ate you alive.")
+            else:
+                print("It didn't understand anything you say and ate you alive.")
+    else:
+        print("River was a diversion. A lion ate you alive.")
